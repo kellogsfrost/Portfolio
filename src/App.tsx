8ddc38c8ddc9059275a2ff658ac4417b93848010
Hayden Schultz <haydenschultz42@gmail.com>
2019-08-29 14:43:20 -0700

    working on layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import cat from './cat.svg';
+import me from './me.jpg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App" >
-      <img className="App-logo" src={cat}/>
       <div className="SideBar">
-        <p>This is the SideBar</p>
+        <div className="SideImage">
+        </div>
       </div>
       <div className="Land">
-      <p>This is the Landing</p>
       </div>
       <div className="AboutMe">
-      <p>This is the About Me</p>
+        This is the About Me
       </div>
       <div className="Projects">
-      <p>This is the Project Page</p>
+        This is the Project Page
       </div>
-
     </div>
   );
 }
