0bdf2c05eab650ae59978b81031661d87bb743d1
Hayden Schultz <haydenschultz42@gmail.com>
2019-09-04 12:01:24 -0700

    Updating style

---
+++ src/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+const About: React.FC = () =>{
+    return(
+        <div className="AboutMe">
+           <p>Hello</p>
+        </div>
+    );
+}
+
+
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import linkdin from './linkdin.svg';
 import envelope from './envelope.svg';
-import file from './file.svg'
-import github from './github.svg'
+import file from './file.svg';
+import github from './github.svg';
 import './App.css';
+import About from './About';
 
 const App: React.FC = () => {
   return (
@@ -15,25 +16,21 @@ const App: React.FC = () => {
         <h2 className="Description">Full-Stack Web Devloper</h2>
         <p>About Me</p>
         <p>Projects</p>
-          <img className="Svg" src={linkdin} alt=""/>
+          <img className="Svg" src={linkdin}  alt=""/>
           <img className="Svg" src={github} alt=""/>
-          <img className="Svg" src={envelope} alt=""/>
+          {/* <img className="Svg" src={envelope} alt=""/> */}
           <img className="Svg" src={file} alt=""/>
       </div>
       <div className="Land">
         <div className="InnerLanding">
           <div className="HomeHeading">Hi, I'm Hayden,</div>
           <div className="SubHeading">
-            <h2>a freelance web developer specialized in
-              <br></br>
-            <strong>front-end and back-endweb development.</strong>
+            <h2 className="SubHeadDescription">a freelance web developer.
             </h2>
           </div>
         </div>
       </div>
-      <div className="AboutMe">
-        This is the About Me
-      </div>
+      {About}
       <div className="Projects">
        {/* <img src="https://www.placecage.com/400/600" className="ProjectImg"/>
        <img src="https://www.placecage.com/400/600" className="ProjectImg"/>
